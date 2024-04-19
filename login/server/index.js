969e95f043a1a04568dfbd80d5f936e53b007a71
Aryan Mohammadi <113422176+Aryan22000@users.noreply.github.com>
2024-04-19 10:24:53 +0300

    Update index.js

--- login/server/index.js
+++ login/server/index.js
@@ -18,24 +18,21 @@ app.post('/register', (req, res)=>
         .catch(err => res.status(400).json('Error:'+ err))
     })
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    AdminsInfoModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                if (user.password === password) {
-                    res.status(200).json({ message: "success" });
-                } else {
-                    res.status(401).json({ message: "wrong password" });
-                }
-            } else {
-                res.status(404).json({ message: "user not found" });
+app.post('/login', (req, res)=>{
+    const {email, password} = req.body;
+    AdminsInfoModel.findOne({email : email})
+    .then(user =>{
+        if(user){
+            if(user.password === password){
+                res.json("success");
+            }else{
+                res.json("wrong password");
             }
-        })
-        .catch(err => {
-            res.status(500).json({ message: "internal server error" });
-        });
-});
+        }else{
+            res.json("user not found");
+        }
+    })
+})
     
 app.listen(3001 , ()=>{
     console.log("Server is running on port 3001");
