d240ec3afcba37a2d31fa96281d77721ab30d73d
Aryan Mohammadi <113422176+Aryan22000@users.noreply.github.com>
2024-04-22 14:41:11 +0300

    change database

--- login/server/index.js
+++ login/server/index.js
@@ -1,39 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const AdminsInfoModel = require("./models/admins")
+const AdminsInfoModel = require("./models/admins");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-//mongodb://localhost:27017
-mongoose.connect("mongodb://127.0.0.1:27017/Admins")
+const DB_USERNAME = process.env.DBUSERNAME;
+const DB_PASSWORD = process.env.DBPASSWORD;
+const CLUSTER = process.env.CLUSTER;
+const DB_NAME = process.env.DB;
 
+// MongoDB Atlas connection string
+const url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
-app.post('/register', (req, res)=>
-    {
-        AdminsInfoModel.create(req.body)
-        .then(AdminsInfo =>  res.json(AdminsInfo))
-        .catch(err => res.status(400).json('Error:'+ err))
-    })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Atlas connected'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
-app.post('/login', (req, res)=>{
-    const {email, password} = req.body;
-    AdminsInfoModel.findOne({email : email})
-    .then(user =>{
-        if(user){
-            if(user.password === password){
-                res.json("success");
-            }else{
-                res.json("wrong password");
+app.post('/register', (req, res) => {
+    AdminsInfoModel.create(req.body)
+        .then(AdminsInfo => res.json(AdminsInfo))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    AdminsInfoModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("success");
+                } else {
+                    res.json("wrong password");
+                }
+            } else {
+                res.json("user not found");
             }
-        }else{
-            res.json("user not found");
-        }
-    })
-})
-    
-app.listen(3001 , ()=>{
+        });
+});
+
+app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
