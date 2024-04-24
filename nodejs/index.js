aa752d60574068527ea783592947d97de8640dae
Aryan Mohammadi <113422176+Aryan22000@users.noreply.github.com>
2024-04-24 15:18:05 +0300

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,29 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const Product = require("./models/Product");
-
 const cars = require("./models/Car");
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-  
-const dbURI =
+const dbURI = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
 
-"mongodb+srv://" + process.env.DBUSERNAME + ":" + process.env.DBPASSWORD + 
-"@" + process.env.CLUSTOR + ".mongodb.net/" + process.env.DB + 
-"?retryWrites=true&w=majority&appName=Cluster0";
-
-
-
-
-  console.log(dbURI);
-  mongoose
+mongoose
   .connect(dbURI)
   .then((result) => {
     console.log("Connected to DB");
@@ -31,15 +20,13 @@ const dbURI =
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log("Listening on " + PORT));
 
-    const PORT = process.env.PORT || 3001;
-
     app.use(express.json());
     app.use(cors());
 
     // Nodemailer transporter setup
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      host : "smtp@gmail.com",
+      host : "smtp.gmail.com",
       port : 587,
       secure : false,
       auth: {
@@ -54,9 +41,9 @@ const dbURI =
         res.json(result);
       } catch (error) {
         console.log(error);
+        res.status(500).send('Error fetching products');
       }
     });
-  
 
     app.post("/contact-us", async (req, res) => {
       const { name, email, message } = req.body;
@@ -85,28 +72,17 @@ const dbURI =
         res.status(500).send('Error sending email');
       }
     });
-    
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   })
   .catch((err) => {
     console.log(err);
   });
 
-
-
-
-
-  app.get("/cars", async (req, res) => {
-    try {
-      const result = await cars.find();
-      res.json(result);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  
-
-
+app.get("/cars", async (req, res) => {
+  try {
+    const result = await cars.find();
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error fetching cars');
+  }
+});
